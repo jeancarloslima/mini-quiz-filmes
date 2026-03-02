406bacde63d1bc2752b615d76c2d73fb43ff2777
Jean Lima <jcoliveiralima19@gmail.com>
2026-03-02 16:10:06 -0300

    Ajustando design da tela resultado

--- src/components/TelaResultado/index.jsx
+++ src/components/TelaResultado/index.jsx
@@ -1,8 +1,10 @@
+import "./tela-resultado.css";
+
 export default function TelaResultado({ pontuacao, total }) {
     return (
         <div className="tela-resultado">
             <h2>Fim de Jogo!</h2>
-            <p>Você acertou {pontuacao} de {total} perguntas.</p>
+            <p>Você acertou <span className="acertos">{pontuacao}</span> de {total} perguntas.</p>
         </div>
     )
 }
